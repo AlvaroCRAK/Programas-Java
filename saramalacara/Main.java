65963fc5a8441be8dc2aea6d937b86e486c1428b
AlvaroCRAK <131735140+AlvaroCRAK@users.noreply.github.com>
2024-03-17 14:40:39 -0500

    Humo

---
+++ saramalacara/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import javax.sound.sampled.*;
+import java.io.File;
+public class Main{
+    public static void main(String[] args){
+        File file = new File("saraMusic.wav");
+        try{
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            new Gui();
+            clip.start();
+
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+            System.out.println("error xd");
+        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException e){
+            e.printStackTrace();
+        }
+    }
+}
